e9e1938b31d0cb3af7762e316a56d0e192706315
Rubel Mahmud <vxlrubel@gmail.com>
2024-01-04 15:53:50 +0600

    initiate main plugin file

---
+++ transfer-visitor.php
@@ -0,0 +1,96 @@
+<?php
+/*
+ * Plugin Name:       Transfer Visitor
+ * Plugin URI:        https://github.com/vxlrubel/transfer-visitor
+ * Description:       The "Transfer Visitor" WordPress plugin facilitates URL redirection management via the WordPress dashboard using REST API endpoints. This plugin enables users to set up redirection pairs from old URLs to new destinations efficiently.
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Rubel Mahmud ( Sujan )
+ * Author URI:        https://github.com/vxlrubel/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       transfer-visitor
+ * Domain Path:       /lang
+ */
+
+
+
+ final class Transfer_Visitor{
+
+    // version
+    private $version = '1.0';
+
+    // create instance
+    private static $instance;
+
+    /**
+     * construct method
+     */
+    public function __construct(){
+
+        // check WordPress version
+        add_action( 'admin_init', [ $this, 'check_wp_version' ] );
+
+        // load text domain
+        add_action( 'plugins_loaded', [ $this, 'load_textdomain' ] );
+        
+    }
+
+    /**
+     * register text domain for this plugin
+     *
+     * @return void
+     */
+    public function load_textdomain(){
+        load_plugin_textdomain( 
+            'transfer-visitor',
+            false,
+            dirname( plugin_basename( __FILE__ ) ) . trailingslashit( '/lang' )
+        );
+    }
+
+    /**
+     * check WordPress version
+     *
+     * @return void
+     */
+    public function check_wp_version(){
+        if ( version_compare( get_bloginfo( 'version' ), '5.2', '<' ) ) {
+            add_action( 'admin_notice', [ $this, 'wp_version_notice' ] );
+        }
+    }
+
+    /**
+     * notice for update WordPress version
+     *
+     * @return void
+     */
+    public function wp_version_notice(){
+
+        $notice_text = 'Transfer Visitor plugin requires WordPress version 5.2 or above. Please update your WordPress installation.';
+        
+        printf(
+            '<div class="notice notice-warning"><p>%s</p></div>',
+            __( $notice_text, 'transfer-visitor' )
+        );
+    }
+
+    /**
+     * create singleton instance
+     *
+     * @return void
+     */
+    public static function init(){
+        if ( is_null( self::$instance ) ){
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+ }
+
+ function transfer_visitor(){
+    return Transfer_Visitor::init();
+ }
+
+ transfer_visitor();
