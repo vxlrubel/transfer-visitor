c9df1c25cb55d403da3d2b185fabf4f9fd746ea4
Rubel Mahmud <vxlrubel@gmail.com>
2024-01-04 19:10:07 +0600

    modify author uri

--- transfer-visitor.php
+++ transfer-visitor.php
@@ -7,7 +7,7 @@
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Rubel Mahmud ( Sujan )
- * Author URI:        https://github.com/vxlrubel/
+ * Author URI:        https://www.linkedin.com/in/vxlrubel/
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       transfer-visitor
