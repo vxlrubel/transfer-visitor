9f3d06cadaf94b29959e3728aabaa12c2dcdf39b
Rubel Mahmud <vxlrubel@gmail.com>
2024-01-04 21:43:43 +0600

    create Template class

---
+++ inc/classes/class-template.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace TV\classes;
+use TV\trait\DB_Table as Transfer_Table;
+
+// derectly access denied
+defined('ABSPATH') || exit;
+
+/**
+ * create a class Template for html stucture template of admin page
+ * @version 1.0
+ * @author Rubel Mahmud <vxlrubel@gmail.com>
+ * @link https://github.com/vxlrubel
+ */
+
+ class Template{
+
+    /**
+     * list page
+     * this method display the main list page
+     *
+     * @return void
+     */
+    public static function list_page(){
+
+    }
+
+    /**
+     * add_new_page
+     * this method use to display the add_new_page stucture
+     *
+     * @return void
+     */
+    public static function add_new_page(){
+
+    }
+
+    /**
+     * options_page
+     * this method use to display the options page
+     *
+     * @return void
+     */
+    public static function options_page(){
+        
+    }
+ }
